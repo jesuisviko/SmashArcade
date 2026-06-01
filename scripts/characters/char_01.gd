a6f10eac6b2fd5ee189aada421e272b8cfed48a8
jesuisviko <victorlachaud@icloud.com>
2026-06-01 19:22:38 +0200

    UPDATE : PREMIERE ANIMATION
    Animation d'IDLE pour lil uzi vert (pour le perso 1)
    
    Mettre les animations en mirroir pour le p2, mais plus tard

--- scripts/characters/char_01.gd
+++ scripts/characters/char_01.gd
@@ -6,4 +6,7 @@ func _ready() -> void:
 	weight_multiplier = 1.0
 	char_speed        = 6.5
 	player_id         = 1
+	var anim_player = $Model/char_01_idle_test/AnimationPlayer
+	anim_player.call_deferred("play", "!Main|mixamo_com|Layer0")
+	anim_player.get_animation("!Main|mixamo_com|Layer0").loop_mode = Animation.LOOP_LINEAR
 	super._ready()
